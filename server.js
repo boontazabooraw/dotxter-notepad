2f116d323b1caa60a8ace28d9144d62af558a1ab
Leon Bryan <leonard.bryan.email@gmail.com>
2025-05-18 13:48:34 +0800

    Server fix

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.static('public'));
 app.use(express.json()); // Enable JSON body parsing
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 //Serving text file
 app.get('/file-content', (req, res) => {
