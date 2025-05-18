a49281c791de646734f9139d9f9e2cbcca7b93cc
Leon Bryan <leonard.bryan.email@gmail.com>
2025-05-18 13:40:35 +0800

    Server fix

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json()); // Enable JSON body parsing
 
-//Serving
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+//Serving text file
 app.get('/file-content', (req, res) => {
     fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (err, data) => {
       if (err) {
