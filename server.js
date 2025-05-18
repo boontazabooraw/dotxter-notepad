901b0238340adb3f5c5ad737900a9901911f9992
Leon Bryan <leonard.bryan.email@gmail.com>
2025-05-18 13:54:47 +0800

    Server fix

--- server.js
+++ server.js
@@ -9,36 +9,36 @@ app.use(express.static('public'));
 app.use(express.json()); // Enable JSON body parsing
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the homepage
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 //Serving text file
 app.get('/file-content', (req, res) => {
-    fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (err, data) => {
-      if (err) {
-        console.error('Read error:', err);
-        return res.status(500).send('Failed to read file');
-      }
-      res.send(data);
-    });
+  fs.readFile(path.join(__dirname, 'data', 'text.txt'), 'utf8', (err, data) => {
+    if (err) {
+      console.error('Read error:', err);
+      return res.status(500).send('Failed to read file');
+    }
+    res.send(data);
   });
+});
 
 //Updating
 app.post('/update-file', (req, res) => {
-    const { content } = req.body;
-    fs.writeFile(path.join(__dirname, 'text.txt'), content, (err) => {
-      if (err) {
-        console.error('Write error:', err);
-        return res.status(500).send('Failed to write file');
-      }
-      res.sendStatus(200);
-    });
+  const { content } = req.body;
+  fs.writeFile(path.join(__dirname, 'data', 'text.txt'), content, (err) => {
+    if (err) {
+      console.error('Write error:', err);
+      return res.status(500).send('Failed to write file');
+    }
+    res.sendStatus(200);
   });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
